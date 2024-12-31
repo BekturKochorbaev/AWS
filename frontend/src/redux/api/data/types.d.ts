45087d189f209ab1c8c567aaa323fa3d4932390a
Ubuntu <ubuntu@ip-172-31-33-164.eu-north-1.compute.internal>
2024-12-31 11:37:36 +0000

    pol

---
+++ frontend/src/redux/api/data/types.d.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+namespace HOTELS {
+  interface hotel_list {
+    id?: string;
+    name_hotel: string;
+    country: string;
+    city: string;
+    images: string[];
+    rating: string;
+    count_review: string;
+  }
+
+  type GetHotelListResponse = IOlympian[];
+  type GetHotelListRequest = void;
+}
